9919bebb1a6139052e51d70b585433023669092e
Akhil Jacob John <akhiljacobjohn@yahoo.com>
2024-10-17 22:45:25 +0530

    Add DTO Classes

--- src/main/java/com/localtym/account_core/dto/AccountRoleDTO.java
+++ src/main/java/com/localtym/account_core/dto/AccountRoleDTO.java
@@ -1,12 +1,10 @@
 package com.localtym.account_core.dto;
 
-import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-@Entity
 public class AccountRoleDTO {
     private String acctnbr;
     private String relationshiptype;
